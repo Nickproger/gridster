5c32c70d439240c8e73ef7af143bc596d0d5d2d0
unknown <nmelnyk@PUAnmk01.matrix42.de>
2014-08-15 15:12:19 +0300

    Commit 2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,70 +21,78 @@ angular
 		}
 	});
 
-	this.gridsterOpts = {};
 
 
-}])
-.directive();
+	this.dataSmall = {
+		c1: [       936, 968, 1025, 999, 998, 1014, 1017, 1010, 1010, 1007,
+                    1004, 988, 990, 988, 987, 995, 946, 954, 991, 984,
+                    974, 956, 986, 936, 955, 1021, 1013, 1005, 958, 953,
+                    952, 940, 937, 980, 966, 965, 928, 916, 910, 980
+        ],
+        c2: [
+                    16, 17, 18, 19, 20, 21, 21, 22, 23, 22,
+                    20, 18, 17, 17, 16, 16, 17, 18, 19, 20,
+                    21, 22, 23, 25, 24, 24, 22, 22, 23, 22,
+                    22, 21, 16, 15, 15, 16, 19, 20, 20, 21
+        ],
+        c3: [
+                    71, 70, 69, 68, 65, 60, 55, 55, 50, 52,
+                    73, 72, 72, 71, 68, 63, 57, 58, 53, 55,
+                    63, 59, 61, 64, 58, 53, 48, 48, 45, 45,
+                    63, 64, 63, 67, 58, 56, 53, 59, 51, 54
+        ]
+    }
 
 
+	/* Temp code: !!! Move to the separate directive !!!!!!!!!!!!! */
+	this.gridsterOpts = {
+		resizable: {
+			start: function(event, uiWidget, $element) {
+				if (!$element.innerChart) {
+					$element.innerChart = $element.find("[kendo-chart]").data("kendoChart");
+				}
+				$element.addClass("resizingMode");
+			},
+			stop: function(event, uiWidget, $element) {
+				// !!! try to find corresponding event, which fires after resizing, adjusting to grid and getting new sizes for tile
+				setTimeout(function () {
+					$element.removeClass("resizingMode");
+					$element.innerChart.resize();
+					//$element.innerChart.refresh();
+				}, 300);
+			}
+		}
+	};
 
+	this.chart1 = {};
+	this.chart2 = {};
+	this.chart3 = {};
 
-
-
-function createChart() {
-    $("#mychart").kendoChart({
-        title: {
-            text: "Gross domestic product growth \n /GDP annual %/"
-        },
-        legend: {
-            position: "bottom"
-        },
-        seriesDefaults: {
-            type: "area",
-            area: {
-                line: {
-                    style: "smooth"
-                }
-            }
-        },
-        series: [{
-            name: "India",
-            data: [3.907, 7.943, 7.848, 9.284, 9.263, 9.801, 3.890, 8.238, 9.552, 6.855]
-        }, {
-            name: "World",
-            data: [1.988, 2.733, 3.994, 3.464, 4.001, 3.939, 1.333, -2.245, 4.339, 2.727]
-        }, {
-            name: "Haiti",
-            data: [-0.253, 0.362, -3.519, 1.799, 2.252, 3.343, 0.843, 2.877, -5.416, 5.590]
-        }],
-        valueAxis: {
-            labels: {
-                format: "{0}%"
-            },
-            line: {
-                visible: false
-            },
-            axisCrossingValue: -10
-        },
-        categoryAxis: {
-            categories: [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011],
-            majorGridLines: {
-                visible: false
-            }
-        },
-        tooltip: {
-            visible: true,
-            format: "{0}%",
-            template: "#= series.name #: #= value #"
-        }
-    });
-}
-
-$(document).ready(createChart);
-$(document).bind("kendo:skinChange", createChart);
-
-$("#mybutton").click(function () {
-	$("#mychart").css("width", "800px");
-	$("#mychart").data("kendoChart").resize();
-});
+}]).directive("chartSizeAutoAdjust", [function () {
+	return {
+		priority: 10000,
+		require: "kendo-chart",
+		restrict: 'A',
+		link: function(scope, element, attrs, kendoChartController) {
+			console.log("gridsterAdjuster !!!!", element.data("kendoChart"), kendoChartController);
+			// temporary hack, it should be redone with correct event
+			setTimeout(function () {
+				element.data("kendoChart").resize();
+			}, 1000);
+		}
+	}
+}]).directive("sparklineSizeAutoAdjust", [function () {
+	return {
+		priority: 10000,
+		require: "kendo-sparkline",
+		restrict: 'A',
+		link: function(scope, element, attrs, kendoSparklineController) {
+			console.log("gridsterAdjuster sparkline !!!!", element.data("kendoSparkline"), kendoSparklineController);
+			// temporary hack, it should be redone with correct event
+			setTimeout(function () {
+				console.log(element.data("kendoSparkline"));
+				element.data("kendoSparkline").resize();
+			}, 1000);
+		}
+	}
+}]);
